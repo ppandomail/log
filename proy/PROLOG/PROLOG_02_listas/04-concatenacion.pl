ee83c6846013c28076121d460435a7498b553bdd
Pablo Pandolfo <ppandomail@gmail.com>
2024-05-27 20:55:51 -0300

    ajuste a ejemplo de lista prolog

--- proy/PROLOG/PROLOG_02_listas/04-concatenacion.pl
+++ proy/PROLOG/PROLOG_02_listas/04-concatenacion.pl
@@ -1,7 +1,7 @@
 % Concatenación de listas
 
 concatenacion([], L, L).
-concatenacion([X,L1], L2, [X,L3]) :- concatenacion(L1, L2, L3).
+concatenacion([X|L1], L2, [X|L3]) :- concatenacion(L1, L2, L3).
 
 % ?- concatenacion([a, b, c], [c, d, e], L).
 % L = [a, b, c, c, d, e]
